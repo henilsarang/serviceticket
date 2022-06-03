f770e2d47082069674127ad88b6783a5e050fab8
henilsarang <henilsarang2716@gmail.com>
2022-06-03 01:18:30 -0400

    Authentication Middleware implemented

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required : [true, 'Please add name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add email'],
+        unique:true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add password'],
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default : false
+    }
+}, {
+    timestamp : true
+}
+)
+module.exports = mongoose.model('User', userSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server...");
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const app = express()
+app.use(express.json())
+
+//Connect to database
+connectDB()
+
+app.use(express.urlencoded({extended:false}))
+app.get('/', (req, res) => {
+    res.status(200).send({ message :  'Welcome to support Desk'})
+})
+app.use('/api/users', require('./routes/usersRoutes'))
+app.use(errorHandler)
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
